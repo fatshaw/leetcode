63da63942b30babb4987148fa2058a32bb3e8918
chaojie.xiao <chaojie.xiao@baidao.com>
2018-05-12 21:35:30 +0800

    delete array

--- array/TwoSum/TwoSumTest.java
+++
@@ -1,8 +0,0 @@
-@Test
-public void testTwoSum() {
-	assert twoSum(new int[]{1, 2, 3}, 2) == null;
-	assert twoSum(new int[]{1, 2, 3, 5}, 3)[0] == 0;
-	assert twoSum(new int[]{1, 2, 3, 5}, 3)[1] == 1;
-	assert twoSum(new int[]{1, 2, 3, 5}, 8)[0] == 2;
-
-}
