b0c08658a561bb7fe05590aac4a87a6ff19d8349
chaojie.xiao <chaojie.xiao@airwallex.com>
2020-04-29 09:58:36 +0800

    update max sub array

--- array/maximum-Subarray/MaxSubArray.java
+++ array/maximum-Subarray/MaxSubArray.java
@@ -1,15 +1,11 @@
 public int maxSubArray(int[] nums) {
-  int max = Integer.MIN_VALUE;
-  int sumEnd = 0;
-  for (int n : nums) {
-      if (sumEnd < 0) {
-          sumEnd = n;
-      } else {
-          sumEnd = sumEnd + n;
-      }
-      if (sumEnd > max) {
-          max = sumEnd;
-      }
-  }
-  return max;
+    if (nums.length == 0) return 0;
+
+    int maxSum = nums[0];
+    int sum = nums[0];
+    for (int i = 1; i < nums.length; i++) {
+        sum = Math.max(sum + nums[i], nums[i]);
+        maxSum = Math.max(sum, maxSum);
+    }
+    return maxSum;
 }
